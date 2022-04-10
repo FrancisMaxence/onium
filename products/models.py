b5858360b40f80b6ce41c32c099e3ef2529fda6c
Francis Maxence <tau.vincentmardon@gmail.com>
2022-04-10 14:17:07 -0400

    Modify products.models.Product

--- products/models.py
+++ products/models.py
@@ -10,14 +10,14 @@ class Product(models.Model):
     supplier_code = models.CharField(max_length=20, null=True, blank=True)
     plu_code = models.IntegerField(unique=True, null=True, blank=True)
     cash_register_code = models.IntegerField(unique=True, null=True, blank=True)
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True, blank=True) # Make it mandatory !
     description = models.CharField(max_length=150)
-    weight = models.DecimalField(max_digits=8, decimal_places=2)
-    measure = models.ForeignKey(Measure, on_delete=models.PROTECT)
-    case_size = models.IntegerField(blank=True, default=1)
-    supplier = models.ForeignKey(Supplier, on_delete=models.PROTECT)
-    department = models.ForeignKey(Department, on_delete=models.PROTECT)
-    in_store = models.BooleanField()
+    weight = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True) # Make it mandatory !
+    measure = models.ForeignKey(Measure, on_delete=models.PROTECT, null=True, blank=True) # Make it mandatory !
+    case_size = models.IntegerField(blank=True, default=1, null=True) # Make it mandatory !
+    supplier = models.ForeignKey(Supplier, on_delete=models.PROTECT, null=True, blank=True) # Make it mandatory !
+    department = models.ForeignKey(Department, on_delete=models.PROTECT, null=True, blank=True) # Make it mandatory !
+    in_store = models.BooleanField(null=True, blank=True) # Make it mandatory !
 
     class Meta:
         ordering = ['brand', 'description']
